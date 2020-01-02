96fceb3d5362c5a2038fd85680fe36885428fc95
GusRiva <gusfer@gmail.com>
2020-01-02 21:11:42 +0100

    some changes

--- js/network1.js
+++ js/network1.js
@@ -2,26 +2,16 @@
     var all_links;
     
     $.getJSON("data/test_data.json", function(json){
-        all_nodes = json['nodes'];
+/*    $.getJSON("data/hsc_d3_data.json", function(json){*/
         all_links = json['links'];
+        all_nodes = json['nodes'];
+        console.log(all_nodes,all_links)
     });
     
 /*    This should be chosen by the user*/
-    var initial_node = 2;
+    var initial_node = "2";
     
-    var a = {id: "a"},
-        b = {id: "b"},
-        c = {id: "c"},
-        nodes = [{"id":2, "name":"B"}],
-        links = [];
-        
-    
-
-
 $(document).ready(function() {
-    if (all_nodes.length > 1) {
-        console.log("Loaded",all_nodes)
-    };
     // set the dimensions and margins of the graph
     var margin = {top: 10, right: 30, bottom: 30, left: 40},
     width = 700 - margin.left - margin.right,
@@ -31,7 +21,32 @@ $(document).ready(function() {
    var svg = d3.select("#my_dataviz").append("svg")
     .attr("width", width)
     .attr("height", height);
+    
+    var nodes = [],
+        links = []
+        visible_nodes = [initial_node] /* List of the nodes that are already in the canvas, so to not add them twice */
 
+/*        This should be important to check that the data loads before starting*/
+/*    var loading = setInterval(function(){
+       if (typeof all_links === 'undefined') {
+            console.log("Not Loaded")
+            $("#my_dataviz").text("Loading");
+        } else {
+            
+            console.log("loaded", nodes);
+            restart();
+            loaded();
+        };    
+    }, 500);
+    
+    function loaded(){
+        clearInterval(loading)
+    };*/
+
+    
+/*    nodes = all_nodes.filter(function(node){return node['id'] === initial_node});*/
+    var nodes = [{id:"2",Name:"B"}]
+    var links = []
     
     var simulation = d3.forceSimulation(nodes)
         .force("charge", d3.forceManyBody().strength(-1000))
@@ -49,21 +64,58 @@ $(document).ready(function() {
     
     toggle_master = 1;
     node.on("click", function(clicked_node) {
-        if (toggle_master === 0 ){
+/*    Adapt this function with the external data*/
+        var filtered_links = all_links.filter(function(lk){
+            return lk['source'] === String(clicked_node['id']) || lk['target'] === String(clicked_node['id'])
+        });
+        
+        console.log(filtered_links)
+        
+        var filtered_nodes_ids = new Set([]);
+           for (lk in filtered_links){
+               filtered_nodes_ids.add(filtered_links[lk]['source']);
+               filtered_nodes_ids.add(filtered_links[lk]['target']);
+            };
+         filtered_nodes_ids = Array.from(filtered_nodes_ids)
+         
+         filtered_nodes_ids = jQuery.grep(filtered_nodes_ids, function(node_id){
+             return jQuery.inArray(node_id,visible_nodes) === -1;
+         })
+                  
+         filtered_nodes = all_nodes.filter(function(nd){
+               return filtered_nodes_ids.includes(nd['id']);
+            });
+         
+         for (nd in filtered_nodes){
+             nodes.push(filtered_nodes[nd]);
+         };
+                
+        
+        /*for (lk in filtered_links){
+              links.push(filtered_links[lk])    
+          };
+        */
+        
+        
+        
+        
+ /*       if (toggle_master === 0 ){
             nodes = [a,b];
             toggle_master = 1;
         } else if (toggle_master === 1 ){
-            links.push({source: a, target: b}); // Add a-b.
-            links.push({source: b, target: c}); // Add b-c.
-            links.push({source: c, target: a}); // Add c-a.
-            nodes.push(c); // Re-add c.
+            links.push({source: 2, target: 3});
+            links.push({source: 1, target: 2}); 
+            nodes.push(a);
+            nodes.push(b);
+            nodes.push(c);
             toggle_master = 2;
         } else if (toggle_master === 2) {
             nodes.pop(); // Remove c.
             links.pop(); // Remove c-a.
             links.pop(); // Remove b-c.
             toggle_master = 1;
-        };
+        };*/
+        
         restart();
     });
 
@@ -71,6 +123,7 @@ $(document).ready(function() {
 
 function restart() {
   // Apply the general update pattern to the nodes.
+  
   node = node.data(nodes, function(d) { return d.id;});
   node.exit().remove();
   node = node.enter().append("circle").attr("fill", function(d) { return color(d.id); }).attr("r", 8).merge(node)
