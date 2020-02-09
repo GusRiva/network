84eaccdaecd2b0263bb93ea23e8b0083a5cd9dae
GusRiva <gusfer@gmail.com>
2020-02-09 23:52:25 +0100

    r

--- data/closes_epic.R
+++ data/closes_epic.R
@@ -3,10 +3,14 @@ library('rlist')
 setwd('C:/xampp/htdocs/d3network/data/')
 input <- fromJSON(file = "hsc_d3_data_final.json")
 
+
 works <- list('w507', 'w485', 'w710', 'w541', 'w93', 'w1016', 'w483', 'w484')
+
+matrix(0L, ncol = length(works), nrow = length(works), dimnames = list(works, works))
+
 
 for (w in works)
 {
   lk = list.search(input['links'], . == w )
-  
+  lk
 }
